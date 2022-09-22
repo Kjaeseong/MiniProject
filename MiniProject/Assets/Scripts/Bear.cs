cce6a522ad963e73dae5fde5c043e7191c542112
Kjaeseong <103081763+Kjaeseong@users.noreply.github.com>
2022-09-22 16:02:57 +0900

    Delete: Input Test Script

---
+++ MiniProject/Assets/Scripts/Bear.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bear : MonoBehaviour
+{
+    public PlayerInput _input;
+
+
+
+    private void FixedUpdate() 
+    {
+        
+    }
+
+
+
+}
