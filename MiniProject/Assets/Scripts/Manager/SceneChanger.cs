a1e1b70d39fbbae06a43db7ed404aa9702c403b7
Kjaeseong <103081763+Kjaeseong@users.noreply.github.com>
2022-09-22 21:02:11 +0900

    Feat: SceneChange Title->MainScene

--- MiniProject/Assets/Scripts/Manager/SceneChanger.cs
+++ MiniProject/Assets/Scripts/Manager/SceneChanger.cs
@@ -5,11 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
-    
+    public void MainScene()
+    {
+        SceneManager.LoadScene("ParkDoilScene");
+    }
 
+    public void OptionScene()
+    {
+        SceneManager.LoadScene("OptionScene");
+    }
 
-
-
-
-
+    public void CreditScene()
+    {
+        SceneManager.LoadScene("CreditScene");
+    }
 }
