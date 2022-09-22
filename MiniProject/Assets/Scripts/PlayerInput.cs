1b14d1a49a780e1a8902cb183cb11c0538e9c9ff
Kjaeseong <103081763+Kjaeseong@users.noreply.github.com>
2022-09-22 15:53:53 +0900

    Feat: Input Test Script
    테스트용 A키 인풋 코드 추가

--- MiniProject/Assets/Scripts/PlayerInput.cs
+++ MiniProject/Assets/Scripts/PlayerInput.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool A;
+
+
+    private void Update() 
     {
-        
+        if(Input.GetKey(KeyCode.A))
+        {
+            A = true;
+        }
+        else
+        {
+            A = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
