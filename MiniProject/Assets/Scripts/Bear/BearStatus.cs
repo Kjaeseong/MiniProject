730fc6ca346d1e692c59bc8ce6697d250bf9eeda
ParkDoil <103029337+ParkDoil@users.noreply.github.com>
2022-09-23 10:00:55 +0900

    [FEATING] BearMovement (~int)

--- MiniProject/Assets/Scripts/Bear/BearStatus.cs
+++ MiniProject/Assets/Scripts/Bear/BearStatus.cs
@@ -1,18 +1,22 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 
+// y: 3.7 ~ -4 x : -5.6 ~ 9.6
 public class BearStatus : MonoBehaviour
 {
     private GameObject _coinPosition;
+    private Rigidbody2D _rigid;
 
+
+    public float MoveDelayTime = 1f;
     private void Start()
     {
+        _rigid = GetComponent<Rigidbody2D>();
         _coinPosition = transform.GetChild(0).gameObject;
 
         StartCoroutine(SpawnCoin());
+        StartCoroutine(BearMove());
     }
 
     IEnumerator SpawnCoin()
@@ -29,6 +33,34 @@ public class BearStatus : MonoBehaviour
                     break;
                 }
             }
+        }
+    }
+
+    IEnumerator BearMove()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(MoveDelayTime);
+            int num = Random.Range(1, 5);
+            switch (num)
+            {
+                case 1: // 위로 이동
+                    if (transform.position.y * 10f * Time.deltaTime <= 3.7)
+                    {
+                        Vector2 moveUp = new Vector2(transform.position.x, transform.position.y * 10f * Time.deltaTime);
+                    }
+                    break;
+                case 2: // 아래로 이동
+
+                    break;
+                case 3: // 왼쪽으로 이동
+
+                    break;
+                case 4: // 오른쪽으로 이동
+
+                    break;
+            }
+
         }
     }
 }
