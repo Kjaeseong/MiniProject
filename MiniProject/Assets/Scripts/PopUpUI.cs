c3b72d064fa1674538996cb37e26ba435db767b9
Kjaeseong <103081763+Kjaeseong@users.noreply.github.com>
2022-09-27 17:37:24 +0900

    Feat: PopUpText CSV 파싱 코드 추가

--- MiniProject/Assets/Scripts/PopUpUI.cs
+++ MiniProject/Assets/Scripts/PopUpUI.cs
@@ -6,18 +6,38 @@ using TMPro;
 public class PopUpUI : MonoBehaviour
 {
     public TextMeshProUGUI Text;
+    public string EventDescription { get; private set; }
 
-    public List<string> TextList = new List<string>();
+    public List<Dictionary<string, object>> TextCSV;
+
+    private void Awake() 
+    {
+        TextCSV = CSVReader.Read ("PopUpText");
+    }
 
     private void OnEnable() 
     {
         Time.timeScale = 0;
-        Text.text = "GGGGGGGGGGGGGG";
+
+        PopUpText("HappyGagueUp");
     }
 
     private void OnDisable() 
     {
         Time.timeScale = 1;
+    }
+
+    public void PopUpText(string eventDescription)
+    {
+        EventDescription = eventDescription;
+
+        for(int i = 0; i < TextCSV.Count; i++)
+        {
+            if(EventDescription == TextCSV[i]["eventDscription"].ToString())
+            {
+                Text.text = TextCSV[i]["Text"].ToString();
+            }
+        }
     }
 
     public void DeActivate()
